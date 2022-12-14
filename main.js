81fa301770abc88ee1f373b96518e607c9c4f281
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-12-14 09:56:47 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// given the following code
+
+function returnName(name) {
+    return name
+}
+
+function consoleLogName(name) {
+    console.log(name)
+}
+
+var name1 = returnName('John')
+var name2 = consoleLogName('Garrett')
+
+// What will the value of name1 be?
+
+
+// What will the value of name2 be?
+
+
+// ===================Pyramid algo
+
+// You must write a function called returnPyramid.
+
+// returnPyramid returns one row of '#' per integer input. The rows follow a particular pattern. The first row is '#'. The second row is '##'. The third row is '###'. So on and so forth.
+
+// Sample output:
+
+// returnPyramid(2); //=> ['#','##']
+// returnPyramid(4); //=> ['#','##','###','####']
+// returnPyramid(6); //=> ['#','##','###','####','#####','######']
+
+function returnPyramid(height) {
+    // your code goes here
+}
+
+// ===================Pyramid algo
+
+// fizzBuzzArray takes a number n as input. As output, fizzBuzzArray returns an array of n numbers following special rules.
+
+// Output array should start at number 1. And should contain n numbers.
+
+// Special rules. If the number is only divisible by three, don't add the number to the return array. Instead, add 'fizz'. If the number is only divisible by five, don't add the number to the return array either. Instead, add 'buzz'. If the number is divisible by both three AND five, don't add the number to the return array. Instead, add 'fizzBuzz'.
+
+// Sample output:
+
+// fizzBuzzArray(15); //=> [1,2,'fizz',4,'buzz','fizz',7,8,'fizz','buzz',11,'fizz',13,14,'fizzBuzz']
+// fizzBuzzArray(100); //=> [1,2,'fizz', ..., 97, 98, 'fizz','buzz']
+
+function fizzBuzz(input) {
+    // your code goes here
+}
+
+
+
+
